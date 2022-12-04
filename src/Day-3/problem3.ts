36ac0820f94f7edf6c493b7d1bc3d785168ee9ea
PalmaAnd <andrepalma@outlook.de>
2022-12-04 20:51:08 +0100

    Added comments

--- src/Day-3/problem3.ts
+++ src/Day-3/problem3.ts
@@ -3,6 +3,7 @@ import * as fs from "fs";
 const input = fs.readFileSync("src/Day-3/input.txt", "utf-8");
 const rucksacks = input.split("\n");
 
+// Generate an array with all letters to later read out the index of it
 const allLetters = generateAlphabetFromTo();
 
 let compartmentA;
@@ -12,30 +13,39 @@ let result = 0;
 
 let elementFound = false;
 
+// Circle trough all entries of the list
 rucksacks.forEach((rucksack) => {
     elementFound = false;
     
+    // Create two string containing both the first and the second half of the string
     compartmentA = rucksack.substring(0, rucksack.length / 2);
     compartmentB = rucksack.substring(rucksack.length / 2);
 
+    // For each letter circle trough both compartments and check if the letter can be found in both
     compartmentA.split("").forEach((element) => {
         if (compartmentB.indexOf(element) >= 0 && !elementFound) {
-            result += allLetters.indexOf(element);
+            // If so, set the bool to true, to stop thee circle
             elementFound = true;
+            // Add the index of the elemtn to the results
+            result += allLetters.indexOf(element);
         }
     });
 });
 
 console.log(result);
 
-
+/**
+ * Helper function used to generate an array with all lower-case and upper-case letters
+ * @returns an array containing all letters
+ */
 function generateAlphabetFromTo(): string[] {
     const letters = "abcdefghijklmnopqrstuvwxyz";
     let alphabet = letters.split("");
     alphabet.forEach((letter) => {
         alphabet.push(letter.toUpperCase());
     });
-
+    /* Unshift adds an element at the beginning of the array, I do this so I 
+     can use the index of the letter without increasing it by one */
     alphabet.unshift("");
     return alphabet;
 }
--- src/Day-3/problem3b.ts
+++ src/Day-3/problem3b.ts
@@ -3,31 +3,40 @@ import * as fs from "fs";
 const input = fs.readFileSync("src/Day-3/input.txt", "utf-8");
 const rucksacks = input.split("\n");
 
+// Generate an array with all letters to later read out the index of it
 const allLetters = generateAlphabetFromTo();
-
 
 let result = 0;
 let elementFound = false;
 
+// Create a for loop to circle trough all entries but always take groups of 3 (for that we have the += 3)
 for (let i = 0; i < rucksacks.length; i += 3) {
     elementFound = false;
-    
-    const first = rucksacks[i];
-    const second = rucksacks[i+1];
-    const third = rucksacks[i+2];
-    
-    first.split('').forEach(letter => {
-        if (second.indexOf(letter) >= 0 && third.indexOf(letter) >= 0 && !elementFound) {
 
+    const first = rucksacks[i];
+    const second = rucksacks[i + 1];
+    const third = rucksacks[i + 2];
+
+    // For each letter we check if it can be found inside one of the other two rucksacks and if no element has been found till now
+    first.split("").forEach((letter) => {
+        if (
+            second.indexOf(!elementFound && letter) >= 0 &&
+            third.indexOf(letter) >= 0
+        ) {
+            // If we find an element set the bool to true to stop the control
             elementFound = true;
-            result +=  allLetters.indexOf(letter) + 1;
+            // Add the index + 1, because arrays begin at 0, to the result
+            result += allLetters.indexOf(letter) + 1;
         }
     });
-    
 }
 
 console.log(result);
 
+/**
+ * Helper function used to generate an array with all lower-case and upper-case letters
+ * @returns an array containing all letters
+ */
 function generateAlphabetFromTo(): string[] {
     const letters = "abcdefghijklmnopqrstuvwxyz";
     let alphabet = letters.split("");
