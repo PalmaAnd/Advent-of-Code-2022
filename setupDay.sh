b18dd4ebd6ebd2bd163505496417821b36ba53b6
PalmaAnd <andrepalma@outlook.de>
2022-12-05 21:12:18 +0100

    Added shellscript to automate the creation of folders and files

---
+++ setupDay.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Creates a folder with the given number at the end.
+# The function also creates two .txt files after creating the folder.
+create_folder_and_files() {
+    read -p "Please enter the number of your day: " DAY
+    if [ ! -d "src/Day-"$DAY ]
+    then
+        mkdir "src/Day-$DAY"
+        touch "src/Day-$DAY/input.txt"
+        touch "src/Day-$DAY/example.txt"
+    fi
+}
+
+###
+# Main body of script starts here
+###
+echo "Welcome to this little script."
+echo "This script will create a folder, with the given number you give it as the ending character."
+echo "Developed by Palma Andrè."
+
+create_folder_and_files
