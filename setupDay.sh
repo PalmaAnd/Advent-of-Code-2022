f5cc614fe81796ffe0e586daccf0d474d94363c5
PalmaAnd <andrepalma@outlook.de>
2022-12-05 21:57:22 +0100

    Added function to shell script

--- setupDay.sh
+++ setupDay.sh
@@ -14,11 +14,26 @@ create_folder_and_files() {
     fi
 }
 
+# This function creates a folder with certain files inside, automatically with
+# the current number of day
+create_folder_and_files_automated() {
+    today="$(date +'%d')"
+    if [ ! -d "src/Day-$today" ]
+    then
+        mkdir "src/Day-$today"
+        touch "src/Day-$today/input.txt"
+        touch "src/Day-$today/example.txt"
+        touch "src/Day-$today/problem$today.ts"
+        touch "src/Day-$today/problem$today-b.ts"
+    fi
+}
+
 ###
 # Main body of script starts here
 ###
 echo "Welcome to this little script."
-echo "This script will create a folder, with the given number you give it as the ending character."
+echo "This script will create a folder, with the given number you give it as the ending character, or directly with the current day."
 echo "Developed by Palma Andrè."
 
-create_folder_and_files
+create_folder_and_files_automated
+# create_folder_and_files
