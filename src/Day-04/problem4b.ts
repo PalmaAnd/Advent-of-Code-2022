5e9890bbd8473383328c6e33bbfdb8df3d42c7e5
PalmaAnd <andrepalma@outlook.de>
2022-12-06 09:30:44 +0100

    Finished day 4

---
+++ src/Day-04/problem4b.ts
@@ -0,0 +1,41 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync("src/Day-04/input.txt", "utf-8");
+const sections = input.split("\n");
+
+let result = 0;
+
+let elfA, elfB;
+
+// Circle trough all entries of the list
+sections.forEach((section) => {
+    section = section.replace("\r", "");
+
+    elfA = section.split(",")[0].split("-");
+
+    elfB = section.split(",")[1].split("-");
+
+    if (
+        parseInt(elfA[0]) <= parseInt(elfB[0]) &&
+        parseInt(elfA[1]) >= parseInt(elfB[1])
+    ) {
+        result++;
+    } else if (
+        parseInt(elfB[0]) <= parseInt(elfA[0]) &&
+        parseInt(elfB[1]) >= parseInt(elfA[1])
+    ) {
+        result++;
+    } else if (
+        parseInt(elfB[0]) <= parseInt(elfA[0]) &&
+        parseInt(elfB[1]) >= parseInt(elfA[0])
+    ) {
+        result++;
+    } else if (
+        parseInt(elfA[0]) <= parseInt(elfB[0]) &&
+        parseInt(elfA[1]) >= parseInt(elfB[0])
+    ) {
+        result++;
+    }
+});
+
+console.log(result);
