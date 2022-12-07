dcea1087a8b20bc955373506ff225e9f4f7fa9f4
PalmaAnd <andrepalma@outlook.de>
2022-12-07 12:32:55 +0100

    Started working on Day5

--- src/Day-05/problem05.ts
+++ src/Day-05/problem05.ts
@@ -1,22 +1,98 @@
 import * as fs from "fs";
 
-const input = fs.readFileSync("src/Day-05/example.txt", "utf-8");
-const sections = input.split("\n");
-
-let commands;
-
 let result = 0;
 
-// Circle trough all entries of the list
-sections.forEach((section) => {
-    section = section.replace("\r", "");
+// Remove all \r for Windows and remove whitespaces and the end
+const input = fs
+    .readFileSync("src/Day-05/example.txt", "utf-8")
+    .replace(/\r/g, "")
+    .trimEnd();
 
+const [givenStacks, givenCommands] = input.split("\n\n");
+
+const stack = createStacks(givenStacks);
+
+const commands = trimCommands(givenCommands);
+
+for (let i = 0; i < commands.from.length; i++) {
+    moveItem(stack, parseInt(commands.move[i]), parseInt(commands.from[i]), parseInt(commands.to[i]));   
+}
+
+
+
+// for (const commands.values of object) {
     
+// }
 
-    console.log("section");
-    console.log(section);
+/**
+ * Helper function to create an object containing the given input string with the crates and their index
+ * transformed to a workable variable
+ * 
+ * @param inputStacks The given string containing all "crates"
+ * @returns An object with all crates mapped to the right index
+ */
+function createStacks(inputStacks: string):object {
+    // ... line makes the function detect all whitespaces and we do index % 4 == 1, because the important values are always on the fourth place
+    const stackValues = inputStacks
+        .split("\n")
+        .map((line) => [...line].filter((val, index) => index % 4 == 1));
+
+    // Get the indexes of each stack
+    const indexes = stackValues.pop();
+
+    // Create an empty object to work with the input data
+    const returnStack = {};
+
+    // For each line of crates given by the stack
+    for (const crateLine of stackValues) {
+        // Go over each element and add it to the according index
+        for (let i = 0; i < crateLine.length; i++) {
+            // If the index does not exist create it as an array
+            if (!returnStack[indexes[i]]) {
+                returnStack[indexes[i]] = [];
+            }
+            // Use unshift to add it at the start
+            returnStack[indexes[i]].unshift(crateLine[i]);
+        }
+    }
+    return returnStack;
+}
+
+/**
+ * Helper function to create a object containing all commands as a variable
+ * 
+ * @param commands String containing all commands
+ * @returns An object with a sorted array containing all move, from, to numbers
+ */
+function trimCommands(commands:string){
+    const commandArray = commands.split("\n");
     
+    const numbers = {
+        "move": [],
+        "from": [],
+        "to": []
 
-});
+    };
+    
+    commandArray.forEach(command => {
+        command = command.replace('move', '')
+                        .replace('from', ',')
+                        .replace('to', ',');
+        const commandNumbers = command.split(",");
 
-console.log(result);
+        for (let index = 0; index < commandNumbers.length; index += 3) {
+            numbers.move.push(commandNumbers[index]);
+            numbers.from.push(commandNumbers[index+1]);
+            numbers.to.push(commandNumbers[index+2]);
+
+
+        }
+    });
+
+    return numbers;
+}
+
+// Function used to move items between stacks
+function moveItem(currentStack:object, move:number, from:number, to:number) {
+    
+}
