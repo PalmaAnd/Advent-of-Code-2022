c011ee8595f9e37f60398cb3da83a859f93e0992
PalmaAnd <andrepalma@outlook.de>
2022-12-21 11:23:38 +0100

    Updated name

---
+++ src/Day-07/problem07.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+
+const input = fs
+    .readFileSync("src/Day-07/example.txt", "utf-8");
+
+let neededCharacters = 0;
+
+let solution = [];
+
+// Go trough each character
+input.split("").forEach(element => {
+    // If there aren't 4 different elements found
+    if (solution.length < 4) {
+        // If a element is found which already is inside the array reset it
+        if (solution.includes(element)) {
+            solution = [];
+        }
+        solution.push(element);
+        neededCharacters++;
+    }
+});
+
+console.log(solution);
+console.log(neededCharacters);
