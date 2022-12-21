42a3171f1730a1fbc59ee34d655e362636faadd7
PalmaAnd <andrepalma@outlook.de>
2022-12-21 19:58:04 +0100

    Continued work on Day 7

--- src/Day-07/problem07.ts
+++ src/Day-07/problem07.ts
@@ -1,19 +1,50 @@
 import * as fs from "fs";
 
+const MAXSIZE = 100000;
+
 const input = fs
-    .readFileSync("src/Day-07/example.txt", "utf-8");
+    .readFileSync("src/Day-07/example.txt", "utf-8")
+    .replace(/\r/g, "");
 
 const commands = input.split("\n");
+
+// bei jedem cd neues Arr
+// bei jedem cd.. aus subarray gehen
+
+let directories = [];
+let directory = {
+    name: "",
+    size: 0,
+};
 
 let solution = 0;
 
 // Go trough each character
-commands.forEach(element => {
-    if (element.valueOf().match(/\d /gm)) {
+commands.forEach((element) => {
+    let currentValue = element.valueOf();
+
+    // If the current command is a command
+    if (currentValue.match(/[$] /gm) && currentValue.includes("cd")) {
+        // Trim the current command so that only the new directory is available
+        const currentDirectory = currentValue.substring(
+            currentValue.indexOf("cd") + 3
+        );
+
+        directories.push(directory);
+        directory = {
+            name: "",
+            size: 0,
+        };
+
+        if (currentDirectory != "..") directory.name = currentDirectory;
+        else {
+            console.log(directories);
+        }
+    } else if (currentValue.match(/\d /gm)) {
         element = element.substring(0, element.indexOf(" "));
-        console.log(element);
+        directory.size += parseInt(element);
     }
-    
 });
 
+console.log(directories);
 console.log(solution);
