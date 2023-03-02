013be0b9f6dc2670560c59ed63fdfc73cf00e285
PalmaAnd <andrepalma@outlook.de>
2023-03-02 23:05:18 +0100

    Continued work

--- src/Day-08/problem08.ts
+++ src/Day-08/problem08.ts
@@ -14,8 +14,26 @@ for (let i = 0; i < rows.length; i++) {
     grid[i] = rows[i].split("");
 }
 
-console.log(grid[1][2]);
+// Check all values on the left, or right
+function checkRowVisible(row:number, value:number):boolean {
+    for (let column = 0; column < rows.length; column++) {
+        if (grid[row][column] > value) return false;
+    }
+    return true;
+}
 
+// Check all values above, or underneath
+function checkColumnVisible(column:number, value:number):boolean {
+    for (let row = 0; row < rows.length; row++) {
+        if (grid[row][column] > value) return false;
+    }
+    return true;
+}
 
+for (let row = 0; row < rows.length; row++) {
+    for (let column = 0; column < rows.length; column++) {
+        if (checkRowVisible(row, grid[row][column]) || checkColumnVisible(column, grid[row][column])) visibleTrees++;
+    }
+}
 
-
+console.log(visibleTrees);
